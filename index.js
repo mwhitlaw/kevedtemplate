1f14513ee4fc9746ec91bb260b5f3bf42e96433a
Mike Whitlaw <mike.whitlaw@gmail.com>
2020-01-13 08:54:25 -0500

    Isolate the merged data logic

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const _ = require('lodash')
 const csv = require('csv-parser')
 const fs = require('fs')
-
 const template = _.template(fs.readFileSync('template.txt', 'utf8'))
+
+const forEachMergedData = (mergedData) => {
+  // here's where process each merged data
+  console.log(mergedData)
+}
 
 fs.createReadStream('data.csv')
 .pipe(csv())
 .on('data', data => { 
-  console.log(template(data))
+  forEachMergedData(template(data))
 })
 .on('end', () => {
   console.log('Finished')
